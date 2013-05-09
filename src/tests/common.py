57c23a5af44f8addc7a3b97f89665d046fe0e7e8
Chaporgin Anton <chapson@yandex-team.ru>
2013-05-09 04:38:11 +0400

    tests

--- src/tests/common.py
+++ src/tests/common.py
@@ -12,18 +12,18 @@ from po_file_checker.base import (
 class CommonTestCase(unittest.TestCase):
     def test_gettings_translations(self):
         pofile = [
-            mock.Mock(translated=True),
-            mock.Mock(translated=False),
-            mock.Mock(translated=True),
+            mock.Mock(translated=lambda: True),
+            mock.Mock(translated=lambda: False),
+            mock.Mock(translated=lambda: True),
         ]
         elements = entries_with_missing_translations(pofile)
-        self.assertTrue(elements.next().translated())
-        self.assertRaises(StopIteration, lambda : elements.next())
+        self.assertFalse(elements.next().translated())
+        self.assertRaises(StopIteration, lambda: elements.next())
 
     def test_missing_keys(self):
         key_name = 'surrogate_key'
         pofile = [
-            mock.Mock(translated=True, msgid=key_name),
+            mock.Mock(translated=lambda: False, msgid=key_name, msgid_plural=key_name),
         ]
         elements = list(missing_keys(pofile))
         self.assertEqual(len(elements), 1)
