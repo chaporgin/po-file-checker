e03436c060ca9e02c8082ae3d66a4d039f1b7586
Chaporgin Anton <chapson@yandex-team.ru>
2013-05-09 04:23:23 +0400

    setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+PACKAGE_ROOT = 'src'
+PACKAGE_NAME = 'po_file_checker'
+
+setup(
+    name=PACKAGE_NAME,
+    version='0.0',
+    description='Check that po-files have no untranslated keys',
+    author_email='chapson@yandex-team.ru',
+    keywords="po-file, build-tools, sanity check",
+    package_dir={'': PACKAGE_ROOT},
+    packages=find_packages(PACKAGE_ROOT, exclude=['tests']),
+    requires=[
+        'polib',
+        'mock',
+    ]
+)
